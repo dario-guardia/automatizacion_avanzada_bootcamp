2e1e78c723e457b04e0716eb82f0222a0be0ec8e
Dario Guardia <Dario.Guardia@tsoftglobal.com>
2021-05-10 21:20:39 -0300

    modulo hotel caso bajo nivel step y feature agregados

---
+++ src/test/java/automationcraft/testcreation/jetsmart/steps/ModuloHoteles_CasoBajoStepG4.java
@@ -0,0 +1,66 @@
+package automationcraft.testcreation.jetsmart.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ModuloHoteles_CasoBajoStepG4 {
+
+    @Given("un usuario que ingresa a la pagina JetSmart")
+    public void un_usuario_que_ingresa_a_la_pagina_jet_smart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("ingresa al formulario de hoteles")
+    public void ingresa_al_formulario_de_hoteles() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("selecciona lugar de hospedaje {string}")
+    public void selecciona_lugar_de_hospedaje(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("selecciona fecha de llegada {string}")
+    public void selecciona_fecha_de_llegada(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("selecciona fecha de vuelta {int} dias a la fecha")
+    public void selecciona_fecha_de_vuelta_dias_a_la_fecha(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("selecciona numero de habitaciones {string}")
+    public void selecciona_numero_de_habitaciones(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("selecciona numero personas adultas {string}")
+    public void selecciona_numero_personas_adultas(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("presiona buscar")
+    public void presiona_buscar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("pagina muestra una leyenda con error de reserva")
+    public void pagina_muestra_una_leyenda_con_error_de_reserva() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+}
