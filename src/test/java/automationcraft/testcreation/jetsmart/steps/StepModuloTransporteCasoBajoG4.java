20e14b3bc21ed14798ca991b0b914152ce8148fa
MarceloLetelierTsoft <marcelo.letelier@tsoftglobal.com>
2021-05-10 20:21:14 -0400

    sub2

--- src/test/java/automationcraft/testcreation/jetsmart/steps/StepModuloTransporteCasoBajoG4.java
+++ src/test/java/automationcraft/testcreation/jetsmart/steps/StepModuloTransporteCasoBajoG4.java
@@ -1,22 +1,15 @@
 package automationcraft.testcreation.jetsmart.steps;
 
 import bctsoft.grupo4.pageobject.pages.JetSmartHomePage;
-import bctsoft.grupo4.pageobject.pages.JetSmartReservaTrasladoInformacionPage;
-import bctsoft.grupo4.pageobject.pages.JetSmartReservaTrasladoPage;
-import bctsoft.grupo4.pageobject.pages.JetSmartReservaTrasladoPagoPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
+import org.testng.Assert;
 
 
 public class StepModuloTransporteCasoBajoG4 {
 
     protected JetSmartHomePage jsHomePage;
-    protected JetSmartReservaTrasladoPage jsReservaPago;
-    protected JetSmartReservaTrasladoPagoPage jsValidarPago;
-    protected JetSmartReservaTrasladoInformacionPage jsInformacionPago;
-
 
     @Given("Ingreso a la pagina JetSmart e ingreso al modulo de transporte")
     public void ingreso_a_la_pagina_jet_smart_e_ingreso_al_modulo_de_transporte() {
@@ -32,25 +25,25 @@ public class StepModuloTransporteCasoBajoG4 {
     @When("Ingreso al modulo de vuelo")
     public void ingreso_al_modulo_de_vuelo() throws InterruptedException {
         jsHomePage.ClickTraslado();
+        jsHomePage.cambiarDeFrame();
         throw new io.cucumber.java.PendingException();
     }
 
     @When("Selecciono recuadro de solo {string}")
-    public void selecciono_recuadro_de_solo(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void selecciono_recuadro_de_solo(String string) throws InterruptedException {
+        jsHomePage.clickCasillaIda();
         throw new io.cucumber.java.PendingException();
     }
 
     @When("Escribo en la casilla lugar de origen de traslado {string}")
-    public void escribo_en_la_casilla_lugar_de_origen_de_traslado(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void escribo_en_la_casilla_lugar_de_origen_de_traslado(String string) throws InterruptedException {
+        jsHomePage.IngresarOrigen("999999999");
         throw new io.cucumber.java.PendingException();
     }
 
     @Then("Valido leyenda {string}")
-    public void valido_leyenda(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void valido_leyenda(String string) throws InterruptedException {
+        Assert.assertEquals("lugar de origen erroneo",jsHomePage.lugarDestinoErroneo());
         throw new io.cucumber.java.PendingException();
     }
-
 }
